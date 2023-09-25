bbd492ea6a1913ca4e5acc7589171d01fbc4af22
gaetan <Nuxnuxx>
2023-09-25 11:15:30 +0200

    add work

--- qualitealgo/td2/exo1.py
+++ qualitealgo/td2/exo1.py
@@ -12,3 +12,13 @@ def rechercheSequentielle(x, tab):
 
 test = rechercheSequentielle(1, tableau)
 print(test)
+
+# Pour le "meilleur des cas" (tableau trié dans l'ordre décroissant) :
+T_meilleur_cas = list(range(1, 101))
+i = rechercheSequentielle(1, T_meilleur_cas)
+print(i)
+
+# Pour le "pire des cas" (tableau trié dans l'ordre décroissant) :
+T_pire_cas = list(range(100, 0, -1))
+i = rechercheSequentielle(101, T_pire_cas)
+print(i)
